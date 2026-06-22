efbcf3d45263ebc35d2a1cf7382b1e20ee101826
illahristuk <novytskiyillia@gmail.com>
2026-06-22 19:03:33 +0200

    add models

---
+++ accounts/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
+from .views import PhoneTokenView
+
+urlpatterns = [
+    path('login/', PhoneTokenView.as_view(), name='login'),
+    path('token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+]
---
+++ menu/models.py
@@ -0,0 +1,69 @@
+from django.db import models
+from django.core.validators import MinValueValidator
+from decimal import Decimal
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True, blank=True)
+    parent = models.ForeignKey(
+        'self',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name='children',
+        )
+    
+    def __str__(self):
+        return self.name
+
+
+class ModifierGroup(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Modifier(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        validators=[MinValueValidator(Decimal('0.00'))],
+        )
+    modifier_group = models.ForeignKey(
+        ModifierGroup,
+        on_delete=models.CASCADE,
+        related_name='modifiers'
+        )
+
+    def __str__(self):
+        return self.name
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        validators=[MinValueValidator(Decimal('0.00'))],
+        )
+    is_active = models.BooleanField(default=True)
+    preparation_time = models.IntegerField()
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name='dishes',
+    )
+    modifier_groups = models.ManyToManyField(
+        ModifierGroup,
+        blank = True,
+        related_name='dishes',
+    )
+
+    def __str__(self):
+        return self.name
+
+
+    
